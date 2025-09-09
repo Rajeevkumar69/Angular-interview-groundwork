b02c497cc466a216180d7e115491c695d79f8f23
Rajeevkumar69 <rk488296@gmail.com>
2025-09-09 14:28:42 +0530

    guard & inteceptor revision

--- src/app/shared/guard/auth-interceptor.ts
+++ src/app/shared/guard/auth-interceptor.ts
@@ -4,28 +4,33 @@ import { catchError, throwError } from "rxjs";
 
 @Injectable()
 
-export class AuthInterceptor implements HttpInterceptor {
+export class AuthInterceptor implements HttpInterceptor{
+     constructor(
+
+     ){}
+
      intercept(req: HttpRequest<any>, next: HttpHandler) {
           const token = localStorage.getItem('token');
 
-          let userAuth = req;
-          if (token) {
-               userAuth = userAuth.clone({
-                    setHeaders: { Authorization: `Bearer ${token}` }
+          let authData = req;
+
+          if(token){
+               authData = req.clone({
+                    setHeaders:{Authorization:`Bearer ${token}`}
                })
           }
 
-          return next.handle(userAuth).pipe(
-               catchError(error => {
-                    if (error.status === 401) {
-                         console.log(`Unauthorized User`);
-
-                    } else if (error.status === 500) {
-                         console.log(`Internal Server Error`);
+          return next.handle(authData).pipe(
+               catchError(error=>{
+                    if(error.status === 401){
+                         console.log('Unauthorized User');
+                    }else if(error.status === 500){
+                         console.log('Internal Server Error');
                     }
 
-                    return throwError(() => error)
+                    return throwError(()=>error)
                })
           )
+          
      }
 }
--- src/app/shared/guard/guard-guard.ts
+++ src/app/shared/guard/guard-guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, GuardResult, MaybeAsync, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
 
 @Injectable({ providedIn: 'root' })
 
 export class AuthGuard implements CanActivate {
-
      constructor(
           private _router: Router
      ) { }
 
      canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-          const userToken = localStorage.getItem('token');
-          if (!userToken) {
+          const token = localStorage.getItem('token');
+
+          if (!token) {
                this._router.navigateByUrl('/login');
                return false;
           } else {
