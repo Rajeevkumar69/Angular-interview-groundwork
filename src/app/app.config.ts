4b13271e423b59413453d740cf3531334ef391fc
Rajeevkumar69 <rk488296@gmail.com>
2025-09-09 10:17:03 +0530

    interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './shared/guard/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: AuthInterceptor, useClass: AuthInterceptor }
   ]
 };
--- src/app/shared/guard/auth-interceptor.ts
+++ src/app/shared/guard/auth-interceptor.ts
@@ -1,26 +1,31 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError, Observable, throwError } from 'rxjs';
+import { HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpHandler } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, throwError } from "rxjs";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+     intercept(req: HttpRequest<any>, next: HttpHandler) {
           const token = localStorage.getItem('token');
 
           let authReq = req;
           if (token) {
                authReq = req.clone({
-                    setHeaders: { Authorization: `Bearer ${token}` }
-               });
+                    setHeaders: { Authrization: `Bearer ${token}` }
+               })
           }
 
           return next.handle(authReq).pipe(
                catchError(error => {
                     if (error.status === 401) {
-                         console.error('Unauthorized User');
+                         console.log(`Unauth... user`);
+                    } else if (error.status === 500) {
+                         console.log(`Internal Server error`);
                     }
-                    return throwError(() => error);
+
+                    return throwError(() => error)
                })
-          );
+          )
+
      }
 }
