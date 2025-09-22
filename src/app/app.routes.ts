a546247c454424d2a49ca8314a57fbf1913c9677
Rajeevkumar69 <rk488296@gmail.com>
2025-09-22 10:15:44 +0530

    app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,59 +1,6 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+
 
 export const routes: Routes = [
-    {
-        path: "",
-        redirectTo: 'home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        // canActivate:[AuthGuard],
-        loadComponent: () => import('./components/component-1/component-1').then(com => com.Component1)
-    },
-    {
-        path: 'form',
-        loadComponent: () => import('./components/component-2/component-2').then(com => com.Component2)
-    },
-    {
-        path: 'component-3',
-        loadComponent: () => import('./components/component-3/component-3').then(com => com.Component3)
-    },
-    {
-        path: 'component-4',
-        loadComponent: () => import('./components/component-4/component-4').then(com => com.Component4)
-    },
-    {
-        path: 'component-5',
-        loadComponent: () => import('./components/component-5/components-5').then(com => com.Components5)
-    },
-    {
-        path:'component-6',
-        loadComponent: () => import('./components/component-6/components-6').then(com => com.Components6)
-    },
-    {
-        path:'component-7',
-        loadComponent:()=> import('./components/component-7/component-7').then(com => com.Component7)
-    },
-    {
-        path:'component-8',
-        loadComponent:()=> import('./components/component-8/component-8').then(com =>com.Component8)
-    },
-    {
-        path:'component-9',
-        loadComponent:()=> import('./components/component-9/component-9').then(com => com.Component9)
-    },
-    {
-        path:'component-10',
-        loadComponent:()=> import('./components/component-10/component-10').then(com => com.Component10)
-    },
-    {
-        path:'component-11',
-        loadComponent:()=> import('./components/component-11/component-11').then(com =>com.Component11)
-    },
-    {
-        path:'component-12',
-        loadComponent:()=> import('./components/component-12/component-12').then(com => com.Component12)
-    }
-
-];
+     {}
+]
